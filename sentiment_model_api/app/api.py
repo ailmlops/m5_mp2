ef55fa61710530501610fc5cb0a2cb7e70c4bf56
ajaytevatia <ajaytevatia@gmail.com>
2023-08-26 02:34:54 -0400

    working api

--- sentiment_model_api/app/api.py
+++ sentiment_model_api/app/api.py
@@ -8,6 +8,12 @@ from fastapi.encoders import jsonable_encoder
 from sentiment_model import __version__ as model_version
 from sentiment_model.predict import make_prediction
 
+from sentiment_model import __version__ as _version
+from sentiment_model.config.core import config
+from sentiment_model.processing.data_manager import load_model, load_tokenizer
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
 from app import __version__, schemas
 from app.config import settings
 
@@ -32,11 +38,26 @@ async def predict(input_data: schemas.MultipleDataInputs) -> Any:
     sentiment classification model
     """
 
-    input_df = pd.DataFrame(jsonable_encoder(input_data.inputs))
+    #input_df = pd.DataFrame(jsonable_encoder(input_data.inputs))
     
-    results = make_prediction(input_data=input_df.replace({np.nan: None}))
+    tokenizer_file_name = f"{config.app_config.tokenizer_save_file}{_version}.json"
+    tokenizer = load_tokenizer(file_name = tokenizer_file_name)
+    
+    text = 'I watched the movie till end'   
+    text = tokenizer.texts_to_sequences([text])
+    
+    #text = tokenizer.texts_to_sequences([input_data.inputs])
+    text_pad = pad_sequences(text, 
+                             maxlen = config.model_config.max_review_length, 
+                             padding= config.model_config.padding_type, 
+                             truncating= config.model_config.truncating_type)
+    
+    results = make_prediction(input_data = text_pad)
+    
+    #results = make_prediction(input_data=input_df.replace({np.nan: None}))
 
-    if results["errors"] is not None:
-        raise HTTPException(status_code=400, detail=json.loads(results["errors"]))
+    # if results["errors"] is not None:
+    #     raise HTTPException(status_code=400, detail=json.loads(results["errors"]))
 
-    return results
+    
+    return {"predictions":results["predictions"][0]}
